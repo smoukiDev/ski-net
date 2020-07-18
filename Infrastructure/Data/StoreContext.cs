081a9515eae982950a82bf4362a1125887603255
srgmx <smakdealcase@gmail.com>
2020-07-18 12:49:50 +0300

    S6M61. Implemented temp decimal to double conversion for Sqlite DB provider.

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 
+using System.Linq;
 using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +20,25 @@ namespace Infrastructure.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // Convert decimal to double
+            if(Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach(var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var props = entityType.ClrType.GetProperties()
+                        .Where(p => p.PropertyType == typeof(decimal));
+
+                    foreach (var property in props)
+                    {
+                        modelBuilder
+                            .Entity(entityType.Name)
+                            .Property(property.Name)
+                            .HasConversion<double>();
+
+                    }
+                }
+            }
         }
     }
 }
