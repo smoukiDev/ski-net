aeb67632a13bf31de9c9de690d7de29039b74228
srgmx <smakdealcase@gmail.com>
2020-07-18 12:49:05 +0300

    S6M59-60. Implemented sorting by name and price.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -34,9 +34,9 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDTO>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDTO>>> GetProducts(string sort)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
             var products = await _productRepo.ListAsync(spec);
             return Ok(
                 _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDTO>>(products)
--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -5,10 +5,36 @@ namespace Core.Specifications
     public class ProductsWithTypesAndBrandsSpecification
        : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification()
+        public ProductsWithTypesAndBrandsSpecification(string sort)
         {
             AddInclude(p => p.ProductType);
             AddInclude(p => p.ProductBrand);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc": 
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    case "nameAsc":
+                        AddOrderBy(p => p.Name);
+                        break;
+                    case "nameDesc":
+                        AddOrderByDescending(p => p.Name);
+                        break;
+                    default:
+                        AddOrderBy(p => p.Id);
+                        break;
+                }
+            }
+            else
+            {
+                AddOrderBy(p => p.Id);
+            }
         }
         public ProductsWithTypesAndBrandsSpecification(int id)
             : base(p => p.Id == id)
