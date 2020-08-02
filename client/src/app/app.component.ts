2720dcef16f39aeea1dcfef383635291cadc5802
srgmx <smakdealcase@gmail.com>
2020-08-02 19:45:14 +0300

    S08M84. Added IProduct & IPagination interfaces.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { VirtualTimeScheduler } from 'rxjs';
@@ -9,10 +11,10 @@ import { VirtualTimeScheduler } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: any[];
+  products: IProduct[];
   constructor(private http: HttpClient){}
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
       this.products = response.data;
       console.log(response);
     }, error => {
