346524fdd9f4a52367fb50371bdc4fe7b355d3a2
srgmx <smakdealcase@gmail.com>
2020-10-30 19:50:59 +0200

    S13. Implemented basket controller and endpoints.

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Core.Interfaces;
+using Core.Entities;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository _basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBusketByIdAsync(string id)
+        {
+            var basket = await _basketRepository.GetBasketAsync(id);
+
+            // TODO: Test
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasketAsync(CustomerBasket basket)
+        {
+            var updatedBusket = await _basketRepository.UpdateBasketAsync(basket);
+            return Ok(updatedBusket);
+        }
+
+        [HttpDelete]
+        public async Task DeleteBasketAsync(string id)
+        {
+            await _basketRepository.DeleteBasketAsync(id);
+        }
+    }
+}
